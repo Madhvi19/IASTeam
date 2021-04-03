27b4af43776d2e967ddc9e1fc2ed05963b77d604
Madhvi19 <madhvi.panchal@students.iiit.ac.in>
2021-04-03 16:58:28 +0530

    added function sendToSLM()

--- src/monitoringUnit.py
+++ src/monitoringUnit.py
@@ -1,26 +1,32 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+from json import loads
+from kafka.errors import KafkaError
 
 
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
 
 
-
-
-def sendToSLM():
+def sendToSLM(serviceName):
+    topicName = "numtest"
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
 
-    #send the name of failed service
-    #get ack 
-    #if not ACKED within some time 
-    # unable to start 
-
-
-
-
+    producer.send(topicName, serviceName.encode()).add_callback(on_send_success).add_errback(on_send_error) #send the name of failed service
 
 def registerServices():
 
-consumer = KafkaConsumer('numtest',
+    consumer = KafkaConsumer('numtest',
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
@@ -45,12 +51,12 @@ def getStatus():
     # exitting -(tims- cur tim ) >10 -> fail   
     if(cur_time-stored_time)> 10:
         # remove the entry from db
-        sendToSLM()
+        sendToSLM(Name of the service)
 
     #otherwise continue
     while(True):
-        monitor the queue contents 
-        remove one by one and match the current time 
+        # monitor the queue contents 
+        # remove one by one and match the current time 
 
 
 
