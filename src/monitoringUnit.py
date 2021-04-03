f9a165cc5f20a0f4f338133280ee2e4a06c5b6dd
Krishnapriya1106 <70127285+Krishnapriya1106@users.noreply.github.com>
2021-04-03 20:42:14 +0530

    Add files via upload

--- src/monitoringUnit.py
+++ src/monitoringUnit.py
@@ -1,10 +1,12 @@
-from time import sleep
-from json import dumps
-from kafka import KafkaProducer
+import pymongo
+from datetime import datetime
 from kafka import KafkaConsumer
-from pymongo import MongoClient
+from kafka import KafkaProducer
+import calendar
+import time
+import threading
 from json import loads
-from kafka.errors import KafkaError
+from json import dumps
 
 
 def on_send_success(record_metadata):
@@ -15,29 +17,44 @@ def on_send_success(record_metadata):
 def on_send_error(excp):
     log.error('I am an errback', exc_info=excp)
 
-
 def sendToSLM(serviceName):
     topicName = "numtest"
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
+
+    #send the name of failed service
+    #get ack 
+    #if not ACKED within some time 
+    # unable to start 
     producer.send(topicName, serviceName).add_callback(on_send_success).add_errback(on_send_error) #send the name of failed service
 
+
+
+
+
 def registerServices():
-
     consumer = KafkaConsumer('numtest',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     group_id='my-group',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group',
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-
-
-
-
-     ##INsert into collections db
-     #i12nsertone 
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    collection = client.numtest.numtest
+    for message in consumer:
+        #print(message)
+        timestamp=str(message.timestamp)
+        #print(message)
+        #print(len(message.value))
+        if len(message.value)==2:
+            mess = message.value['number']
+            name = message.value['name']
+            mes={"num":mess,"time":timestamp,"name":name}
+            collection.insert_one(mes)                      ##INsert into collections db
+            #print('{} added to {}'.format(mes, collection))
+     
 
 
 
@@ -45,20 +62,36 @@ def registerServices():
 #in a thread
 
 def getStatus():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["numtest"]
+    mycol=mydb["numtest"]
 
-    #reads the collection from the db. onr by one.
-    # exitting -(tims- cur tim ) >10 -> fail   
-    if(cur_time-stored_time)> 10:
-        # remove the entry from db
-        sendToSLM(Name of the service)
-
-    #otherwise continue
-    while(True):
-        # monitor the queue contents 
-        # remove one by one and match the current time 
-
-
-
-
+    while True:
+        now=str(datetime.now())
+        n=now.split(".")
+        for y in mycol.find():
+            ts=datetime.fromtimestamp(int(str(y["time"])[0:-3]))
+            fmt = '%Y-%m-%d %H:%M:%S'
+            tstamp1 = datetime.strptime(str(n[0]), fmt)
+            tstamp2 = datetime.strptime(str(ts), fmt)
+            if tstamp1 > tstamp2:
+                td = tstamp1 - tstamp2
+            else:
+                td = tstamp2 - tstamp1
+            td_mins = int(round(td.total_seconds()))
+            #print(td_mins)
+            if td_mins>80:
+                mycol.delete_one(y)
+                #print(y["name"])
+                my_dict={"name":y["name"]}
+                sendToSLM(my_dict)
+        
 
 #Acts as pub and sends data to the SLM's topic in case of node failure
+
+if __name__=="__main__":
+    th=threading.Thread(target=registerServices)
+    th.start()
+    #th.join()
+    tq=threading.Thread(target=getStatus)
+    tq.start()
